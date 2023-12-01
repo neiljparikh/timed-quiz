cdc861cfb77d5564bee0ac951faa8e3f09094929
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2023-11-30 19:48:08 -0500

    Built HTML structure and question bank

---
+++ Assets/js/script.js
@@ -0,0 +1,33 @@
+//DEPENDENCIES
+
+
+
+
+//DATA
+var questions = [
+    {
+      question: "What is the capital of France?",
+      choices: ["Berlin", "Paris", "Madrid", "Rome"],
+      correctAnswer: "Paris"
+    },
+    {
+      question: "What is the capital of Japan?",
+      choices: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
+      correctAnswer: "Tokyo"
+    },
+    {
+      question: "What is the capital of Brazil?",
+      choices: ["Buenos Aires", "Rio de Janeiro", "Brasília", "São Paulo"],
+      correctAnswer: "Brasília"
+    },
+    {
+      question: "What is the capital of Australia?",
+      choices: ["Sydney", "Melbourne", "Canberra", "Perth"],
+      correctAnswer: "Canberra"
+    },
+    {
+      question: "What is the capital of Italy?",
+      choices: ["Tuscany", "Naples", "Rome", "Milan"],
+      correctAnswer: "Rome"
+    }
+  ];
